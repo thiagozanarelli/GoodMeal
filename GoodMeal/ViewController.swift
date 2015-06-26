6f58177ca37395c6afbda25cdd12fbc716f516a7
thiago zanarelli <thiagozanarelli@MacBook-Pro-de-thiago.local>
2015-06-26 20:54:06 -0300

    Initial Commit

---
+++ GoodMeal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodMeal
+//
+//  Created by thiago zanarelli on 26/06/15.
+//  Copyright (c) 2015 TI IT ZanaLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
